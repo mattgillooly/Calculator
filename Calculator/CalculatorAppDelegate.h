5022e8d0f5e8cd9a3fe5b18f34285429f0e6c58b
Matt Gillooly <matt@mattgillooly.com>
2012-07-29 15:42:00 -0400

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Matt Gillooly on 7/29/12.
+//  Copyright (c) 2012 Matt Gillooly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Matt Gillooly on 7/29/12.
+//  Copyright (c) 2012 Matt Gillooly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
